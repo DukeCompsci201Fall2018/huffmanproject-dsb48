5d833c3f8c98e90a517d241356ad501b5f51be1b
Davis Berlind <davis.berlind@gmail.com>
2018-12-08 17:18:18 -0500

    db rd 3

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -122,10 +122,10 @@ public class HuffProcessor {
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
 			String code = encodings[val];
-		    out.writeBits(code.length(), Integer.parseInt(code,2));
+		    out.writeBitsLong(code.length(), Long.parseLong(code,2));
 		}
 		String code = encodings[PSEUDO_EOF];
-		out.writeBits(code.length(), Integer.parseInt(code,2));
+		out.writeBitsLong(code.length(), Long.parseLong(code,2));
 	}
 	
 	/**
